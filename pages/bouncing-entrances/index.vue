234e3d2967c5f1e7cf72c56507b74634feca5167
Samuel DIDIER-LAURENT <iStuffs@users.noreply.github.com>
2024-05-09 15:35:31 +0200

    add bouncing entrances keyframes

---
+++ pages/bouncing-entrances/index.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <BreakCrumbs />
+    <h1>Bouncing entrances</h1>
+    <h2>Status</h2>
+    <ul>
+      <li>
+        <NuxtLink to="/bouncing-entrances/bounce-in">Bounce in</NuxtLink>
+        <input type="checkbox" checked disabled>
+      </li>
+      <li>
+        <NuxtLink to="/bouncing-entrances/bounce-in-down">Bounce in down</NuxtLink>
+        <input type="checkbox" checked disabled>
+      </li>
+      <li>
+        <NuxtLink to="/bouncing-entrances/bounce-in-left">Bounce in left</NuxtLink>
+        <input type="checkbox" checked disabled>
+      </li>
+      <li>
+        <NuxtLink to="/bouncing-entrances/bounce-in-right">Bounce in right</NuxtLink>
+        <input type="checkbox" checked disabled>
+      </li>
+      <li>
+        <NuxtLink to="/bouncing-entrances/bounce-in-up">Bounce in up</NuxtLink>
+        <input type="checkbox" checked disabled>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
