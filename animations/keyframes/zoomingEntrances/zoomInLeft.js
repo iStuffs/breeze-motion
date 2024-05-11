120684b05ac4ae23324e7c076d2702e46420c04e
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2024-05-11 16:02:34 +0200

    add zooming entrances keyframes

--- animations/keyframes/zoomingEntrances/zoomInLeft.js
+++ animations/keyframes/zoomingEntrances/zoomInLeft.js
@@ -1,17 +1,18 @@
-@keyframes zoomInLeft {
-  from {
-    opacity: 0;
-    transform: scale3d(0.1, 0.1, 0.1) translate3d(-1000px, 0, 0);
-    animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
-  }
 
-  60% {
-    opacity: 1;
-    transform: scale3d(0.475, 0.475, 0.475) translate3d(10px, 0, 0);
-    animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1);
-  }
-}
+// https://github.com/animate-css/animate.css/blob/main/source/zooming_entrances/zoomInLeft.css
 
-.zoomInLeft {
-  animation-name: zoomInLeft;
-}
+const zoomInLeft = [
+  {
+    opacity: 0,
+    transform: `scale3d(0.1, 0.1, 0.1) translate3d(-1000px, 0, 0)`,
+    easing: `cubic-bezier(0.55, 0.055, 0.675, 0.19)`,
+  },
+  {
+    opacity: 1,
+    transform: `scale3d(0.475, 0.475, 0.475) translate3d(10px, 0, 0)`,
+    easing: `cubic-bezier(0.175, 0.885, 0.32, 1)`,
+    offset: 0.6,
+  }
+]
+
+export default zoomInLeft
--- animations/keyframes/zoomingExits/zoomOut.js
+++ animations/keyframes/zoomingExits/zoomOut.js
@@ -1,18 +1,17 @@
-@keyframes zoomOut {
-  from {
-    opacity: 1;
-  }
+// https://github.com/animate-css/animate.css/blob/main/source/zooming_exits/zoomOut.css
 
-  50% {
-    opacity: 0;
-    transform: scale3d(0.3, 0.3, 0.3);
+const zoomOut =[
+  {
+    opacity: 1,
+  },
+   {
+    opacity: 0,
+    transform: scale3d(0.3, 0.3, 0.3),
+    offset: 0.5,
+  },
+  {
+    opacity: 0,
   }
+]
 
-  to {
-    opacity: 0;
-  }
-}
-
-.zoomOut {
-  animation-name: zoomOut;
-}
+export default zoomOut
