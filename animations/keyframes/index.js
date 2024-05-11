77025977733824015f32d2f5e3617ddc7e882b02
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2024-05-12 00:50:31 +0200

    update package.json + export

---
+++ animations/keyframes/index.js
@@ -0,0 +1,206 @@
+/* Attention Seakers */
+import bounce from './attentionSeakers/bounce'
+import flash from './attentionSeakers/flash'
+import headShake from './attentionSeakers/headShake'
+import heartBeat from './attentionSeakers/heartBeat'
+import jello from './attentionSeakers/jello'
+import pulse from './attentionSeakers/pulse'
+import rubberBand from './attentionSeakers/rubberBand'
+import shake from './attentionSeakers/shake'
+import shakeX from './attentionSeakers/shakeX'
+import shakeY from './attentionSeakers/shakeY'
+import swing from './attentionSeakers/swing'
+import tada from './attentionSeakers/tada'
+import wobble from './attentionSeakers/wobble'
+
+/* Back Entrances */
+import backInDown from './backEntrances/backInDown'
+import backInLeft from './backEntrances/backInLeft'
+import backInRight from './backEntrances/backInRight'
+import backInUp from './backEntrances/backInUp'
+
+/* Back Exits */
+import backOutDown from './backExits/backOutDown'
+import backOutLeft from './backExits/backOutLeft'
+import backOutRight from './backExits/backOutRight'
+import backOutUp from './backExits/backOutUp'
+
+/* Fading Entrances */
+import fadeIn from './fadingEntrances/fadeIn'
+import fadeInBottomLeft from './fadingEntrances/fadeInBottomLeft'
+import fadeInBottomRight from './fadingEntrances/fadeInBottomRight'
+import fadeInDown from './fadingEntrances/fadeInDown'
+import fadeInDownBig from './fadingEntrances/fadeInDownBig'
+import fadeInLeft from './fadingEntrances/fadeInLeft'
+import fadeInLeftBig from './fadingEntrances/fadeInLeftBig'
+import fadeInRight from './fadingEntrances/fadeInRight'
+import fadeInRightBig from './fadingEntrances/fadeInRightBig'
+import fadeInTopLeft from './fadingEntrances/fadeInTopLeft'
+import fadeInTopRight from './fadingEntrances/fadeInTopRight'
+import fadeInUp from './fadingEntrances/fadeInUp'
+import fadeInUpBig from './fadingEntrances/fadeInUpBig'
+
+/* Fading Exits */
+import fadeOut from './fadingExits/fadeOut'
+import fadeOutBottomLeft from './fadingExits/fadeOutBottomLeft'
+import fadeOutBottomRight from './fadingExits/fadeOutBottomRight'
+import fadeOutDown from './fadingExits/fadeOutDown'
+import fadeOutDownBig from './fadingExits/fadeOutDownBig'
+import fadeOutLeft from './fadingExits/fadeOutLeft'
+import fadeOutLeftBig from './fadingExits/fadeOutLeftBig'
+import fadeOutRight from './fadingExits/fadeOutRight'
+import fadeOutRightBig from './fadingExits/fadeOutRightBig'
+import fadeOutTopLeft from './fadingExits/fadeOutTopLeft'
+import fadeOutTopRight from './fadingExits/fadeOutTopRight'
+import fadeOutUp from './fadingExits/fadeOutUp'
+import fadeOutUpBig from './fadingExits/fadeOutUpBig'
+
+/* Flippers */
+import flip from './flippers/flip'
+import flipInX from './flippers/flipInX'
+import flipInY from './flippers/flipInY'
+import flipOutX from './flippers/flipOutX'
+import flipOutY from './flippers/flipOutY'
+
+/* Lightspeed */
+import lightSpeedInLeft from './lightspeed/lightSpeedInLeft'
+import lightSpeedInRight from './lightspeed/lightSpeedInRight'
+import lightSpeedOutLeft from './lightspeed/lightSpeedOutLeft'
+import lightSpeedOutRight from './lightspeed/lightSpeedOutRight'
+
+/* Rotating entrances */
+import rotateIn from './rotatingEntrances/rotateIn'
+import rotateInDownLeft from './rotatingEntrances/rotateInDownLeft'
+import rotateInDownRight from './rotatingEntrances/rotateInDownRight'
+import rotateInUpLeft from './rotatingEntrances/rotateInUpLeft'
+import rotateInUpRight from './rotatingEntrances/rotateInUpRight'
+
+/* Rotating exits */
+import rotateOut from './rotatingExits/rotateOut'
+import rotateOutDownLeft from './rotatingExits/rotateOutDownLeft'
+import rotateOutDownRight from './rotatingExits/rotateOutDownRight'
+import rotateOutUpLeft from './rotatingExits/rotateOutUpLeft'
+import rotateOutUpRight from './rotatingExits/rotateOutUpRight'
+
+/* Slinding entrances */
+import slideInDown from 'slideInDown'
+import slideInLeft from 'slideInLeft'
+import slideInRight from 'slideInRight'
+import slideInUp from 'slideInUp'
+
+/* Sliding exits */
+import slideOutDown from './slidingExits/slideOutDown'
+import slideOutLeft from './slidingExits/slideOutLeft'
+import slideOutRight from './slidingExits/slideOutRight'
+import slideOutUp from './slidingExits/slideOutUp'
+
+/* Specials */
+import hinge from './specials/hinge'
+import jackInTheBox from './specials/jackInTheBox'
+import rollIn from './specials/rollIn'
+import rollOut from './specials/rollOut'
+
+/* Zooming entrances */
+import zoomIn from './zoomingEntrances/zoomIn'
+import zoomInDown from './zoomingEntrances/zoomInDown'
+import zoomInLeft from './zoomingEntrances/zoomInLeft'
+import zoomInRight from './zoomingEntrances/zoomInRight'
+import zoomInUp from './zoomingEntrances/zoomInUp'
+
+/* Zooming exits */
+import zoomOut from 'zoomOut'
+import zoomOutDown from 'zoomOutDown'
+import zoomOutLeft from 'zoomOutLeft'
+import zoomOutRight from 'zoomOutRight'
+import zoomOutUp from 'zoomOutUp'
+
+export default {
+  bounce,
+  flash,
+  headShake,
+  heartBeat,
+  jello,
+  pulse,
+  rubberBand,
+  shake,
+  shakeX,
+  shakeY,
+  swing,
+  tada,
+  wobble,
+  backInDown,
+  backInLeft,
+  backInRight,
+  backInUp,
+  backOutDown,
+  backOutLeft,
+  backOutRight,
+  backOutUp,
+  fadeIn,
+  fadeInBottomLeft,
+  fadeInBottomRight,
+  fadeInDown,
+  fadeInDownBig,
+  fadeInLeft,
+  fadeInLeftBig,
+  fadeInRight,
+  fadeInRightBig,
+  fadeInTopLeft,
+  fadeInTopRight,
+  fadeInUp,
+  fadeInUpBig,
+  fadeOut,
+  fadeOutBottomLeft,
+  fadeOutBottomRight,
+  fadeOutDown,
+  fadeOutDownBig,
+  fadeOutLeft,
+  fadeOutLeftBig,
+  fadeOutRight,
+  fadeOutRightBig,
+  fadeOutTopLeft,
+  fadeOutTopRight,
+  fadeOutUp,
+  fadeOutUpBig,
+  flip,
+  flipInX,
+  flipInY,
+  flipOutX,
+  flipOutY,
+  lightSpeedInLeft,
+  lightSpeedInRight,
+  lightSpeedOutLeft,
+  lightSpeedOutRight,
+  rotateIn,
+  rotateInDownLeft,
+  rotateInDownRight,
+  rotateInUpLeft,
+  rotateInUpRight,
+  rotateOut,
+  rotateOutDownLeft,
+  rotateOutDownRight,
+  rotateOutUpLeft,
+  rotateOutUpRight,
+  slideInDown,
+  slideInLeft,
+  slideInRight,
+  slideInUp,
+  slideOutDown,
+  slideOutLeft,
+  slideOutRight,
+  slideOutUp,
+  hinge,
+  jackInTheBox,
+  rollIn,
+  rollOut,
+  zoomIn,
+  zoomInDown,
+  zoomInLeft,
+  zoomInRight,
+  zoomInUp,
+  zoomOut,
+  zoomOutDown,
+  zoomOutLeft,
+  zoomOutRight,
+  zoomOutUp,
+}
