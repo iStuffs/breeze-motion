9c0c6a2e75bf0e947a7c56b992ddb3a918aac45d
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2024-05-22 23:26:14 +0200

    fix attention seekers keyframes

--- animations/keyframes/attentionSeakers/swing.js
+++ animations/keyframes/attentionSeakers/swing.js
@@ -2,26 +2,31 @@
 
 const swing = [
   {
-    transformOrigin: 'top center',
+    transformOrigin: `top center`,
   },
   {
     transform: `rotate3d(0, 0, 1, 15deg)`,
+    transformOrigin: `top center`,
     offset: 0.2
   },
   {
     transform: `rotate3d(0, 0, 1, -10deg)`,
+    transformOrigin: `top center`,
     offset: 0.4
   },
   {
     transform: `rotate3d(0, 0, 1, 5deg)`,
+    transformOrigin: `top center`,
     offset: 0.6,
   },
   {
     transform: `rotate3d(0, 0, 1, -5deg)`,
+    transformOrigin: `top center`,
     offset: 0.8,
   },
   {
     transform: `rotate3d(0, 0, 1, 0deg)`,
+    transformOrigin: `top center`,
     offset: 1,
   }
 ]
